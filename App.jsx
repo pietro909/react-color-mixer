da2cdce113fcc478dba7dda2b7cefbc3d9aa1f9b
pigr2 <pietro.grandi@betssongroup.com>
2016-09-19 11:58:58 +0200

    my first component and two color pickers on the page. Cool

--- App.jsx
+++ App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import ColorPicker from './ColorPicker.jsx';
 
 class App extends React.Component {
     render() {
         return (
-            <div>
-                hello World!!!
-            </div>
+            <article>
+                <section>
+                    <h1>Color mixer</h1>
+                </section>
+                <section>
+                    <ColorPicker/>
+                    <ColorPicker/>
+                </section>
+            </article>
         );
     }
 }
---
+++ ColorPicker.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ColorPicker extends React.Component {
+    render() {
+        return (
+            <div>
+                <input type="color"/>
+            </div>
+        )
+    }
+}
+
+export default ColorPicker;
