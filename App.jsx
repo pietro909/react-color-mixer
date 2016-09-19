a8839d5374d929f87d9b92cd7f900db8a7fdc60f
pigr2 <pietro.grandi@betssongroup.com>
2016-09-19 10:38:04 +0200

    my first hello world with ReactJS!

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                hello World!!!
+            </div>
+        );
+    }
+}
+
+export default App;
